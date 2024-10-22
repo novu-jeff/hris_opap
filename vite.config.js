3f9307af6eb840ff54b54072e37de5c1f994cec4
Carl <iamcarlllemos@gmail.com>
2024-10-22 11:29:52 +0800

    add profile

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,13 @@ import laravel from 'laravel-vite-plugin';
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.js'],
+            input: [
+                'resources/sass/app.scss', 
+                'resources/sass/auth.scss',
+                'resources/sass/admin-layout.scss',
+                'resources/sass/home-layout.scss',
+                'resources/js/app.js'
+            ],
             refresh: true,
         }),
     ],
