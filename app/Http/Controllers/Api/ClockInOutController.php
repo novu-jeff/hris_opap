2b3cf9141c1c7cd81b50cd6b0d19cadfbc69af8a
Carl Llemos <iamcarlllemos@gmail.com>
2024-11-18 17:44:13 +0800

    bugs fixed

--- app/Http/Controllers/Api/ClockInOutController.php
+++ app/Http/Controllers/Api/ClockInOutController.php
@@ -159,7 +159,7 @@ class ClockInOutController extends Controller
         if ($response->successful()) {
             $locationData = $response->json();
     
-            if (env('APP_ENV') === 'local') {
+            if ($locationData) {
                 return 'running in local environment';
             }
     
