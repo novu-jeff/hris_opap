747e1ab71bd2416a2c6c813d82cecc543654db45
Carl Llemos <iamcarlllemos@gmail.com>
2024-12-02 11:35:59 +0800

    changes made

--- resources/views/livewire/admin/ess/clock-in-out/index.blade.php
+++ resources/views/livewire/admin/ess/clock-in-out/index.blade.php
@@ -5,8 +5,10 @@
                 <th>ID</th>
                 <th>Biometrics</th>
                 <th>Name</th>
-                <th>Clock In</th>
-                <th>Clock Out</th>
+                <th>Clock In Time</th>
+                <th>Catured Clock In</th>
+                <th>Clock Out Time</th>
+                <th>Captured Clouck Out</th>
                 <th>Hours Consumed</th>
                 <th>Date</th>
             </tr>
@@ -18,7 +20,13 @@
                     <td>{{ $log->information->biometrics_id }}</td>
                     <td>{{ $log->information->personal->firstname . ' ' . $log->information->personal->lastname }}</td> 
                     <td>{{ \Carbon\Carbon::parse($log->clock_in)->format('h:i A') }}</td>
+                    <td>
+                        <img src="{{ $log->captured_image_clockin ? asset('storage/clockinout/' . $log->captured_image_clockin) : 'https://placehold.co/200x100.png?text=No+Image' }}" alt="Clock In Image">
+                    </td>
                     <td>{{ $log->clock_out ? \Carbon\Carbon::parse($log->clock_out)->format('h:i A') : 'In Progress...' }}</td>
+                    <td>
+                        <img src="{{ $log->captured_image_clockout ? asset('storage/clockinout/' . $log->captured_image_clockout) : 'https://placehold.co/200x100.png?text=No+Image' }}" alt="Clock Out Image">
+                    </td>
                     <td>
                         @if ($log->clock_in && $log->clock_out)
                             @php
