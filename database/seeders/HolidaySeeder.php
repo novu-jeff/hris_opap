2a379bcd207b03eaaba4b91ec66ff244f5f39da9
Carl Llemos <iamcarlllemos@gmail.com>
2025-02-12 16:28:09 +0800

    latest updates

--- database/seeders/HolidaySeeder.php
+++ database/seeders/HolidaySeeder.php
@@ -11,31 +11,31 @@ class HolidaySeeder extends Seeder
     {
         $holidays = [
             // Regular Holidays
-            ['name' => "New Year’s Day", 'type' => "regular", 'date' => "01-01", 'isActive' => true, 'isYearly' => true],
-            ['name' => "Araw ng Kagitingan", 'type' => "regular", 'date' => "04-09", 'isActive' => true, 'isYearly' => true],
-            ['name' => "Maundy Thursday", 'type' => "regular", 'date' => "04-17", 'isActive' => true, 'isYearly' => false],
-            ['name' => "Good Friday", 'type' => "regular", 'date' => "04-18", 'isActive' => true, 'isYearly' => false],
-            ['name' => "Labor Day", 'type' => "regular", 'date' => "05-01", 'isActive' => true, 'isYearly' => true],
-            ['name' => "Independence Day", 'type' => "regular", 'date' => "06-12", 'isActive' => true, 'isYearly' => true],
-            ['name' => "National Heroes Day", 'type' => "regular", 'date' => "08-25", 'isActive' => true, 'isYearly' => false],
-            ['name' => "Bonifacio Day", 'type' => "regular", 'date' => "11-30", 'isActive' => true, 'isYearly' => true],
-            ['name' => "Christmas Day", 'type' => "regular", 'date' => "12-25", 'isActive' => true, 'isYearly' => true],
-            ['name' => "Rizal Day", 'type' => "regular", 'date' => "12-30", 'isActive' => true, 'isYearly' => true],
+            ['name' => "New Year’s Day", 'type' => "regular", 'date' => "01-01", 'isDeleted' => false],
+            ['name' => "Araw ng Kagitingan", 'type' => "regular", 'date' => "04-09", 'isDeleted' => false],
+            ['name' => "Maundy Thursday", 'type' => "regular", 'date' => "04-17", 'isDeleted' => false, 'isYearly' => false],
+            ['name' => "Good Friday", 'type' => "regular", 'date' => "04-18", 'isDeleted' => false, 'isYearly' => false],
+            ['name' => "Labor Day", 'type' => "regular", 'date' => "05-01", 'isDeleted' => false],
+            ['name' => "Independence Day", 'type' => "regular", 'date' => "06-12", 'isDeleted' => false],
+            ['name' => "National Heroes Day", 'type' => "regular", 'date' => "08-25", 'isDeleted' => false, 'isYearly' => false],
+            ['name' => "Bonifacio Day", 'type' => "regular", 'date' => "11-30", 'isDeleted' => false],
+            ['name' => "Christmas Day", 'type' => "regular", 'date' => "12-25", 'isDeleted' => false],
+            ['name' => "Rizal Day", 'type' => "regular", 'date' => "12-30", 'isDeleted' => false],
             
             // Special Non-Working Days
-            ['name' => "Ninoy Aquino Day", 'type' => "special-non-working", 'date' => "08-21", 'isActive' => true, 'isYearly' => true],
-            ['name' => "All Saints Day", 'type' => "special-non-working", 'date' => "11-01", 'isActive' => true, 'isYearly' => true],
-            ['name' => "Feast of the Immaculate Conception of Mary", 'type' => "special-non-working", 'date' => "12-08", 'isActive' => true, 'isYearly' => true],
-            ['name' => "Last Day of the Year", 'type' => "special-non-working", 'date' => "12-31", 'isActive' => true, 'isYearly' => true],
+            ['name' => "Ninoy Aquino Day", 'type' => "special-non-working", 'date' => "08-21", 'isDeleted' => false],
+            ['name' => "All Saints Day", 'type' => "special-non-working", 'date' => "11-01", 'isDeleted' => false],
+            ['name' => "Feast of the Immaculate Conception of Mary", 'type' => "special-non-working", 'date' => "12-08", 'isDeleted' => false],
+            ['name' => "Last Day of the Year", 'type' => "special-non-working", 'date' => "12-31", 'isDeleted' => false],
             
             // Special Working Day
-            ['name' => "EDSA People Power Revolution Anniversary", 'type' => "special-working", 'date' => "02-25", 'isActive' => true, 'isYearly' => true],
+            ['name' => "EDSA People Power Revolution Anniversary", 'type' => "special-working", 'date' => "02-25", 'isDeleted' => false],
             
             // Additional Special (Non-Working) Days
-            ['name' => "Chinese New Year", 'type' => "special-non-working", 'date' => "01-29", 'isActive' => true, 'isYearly' => false],
-            ['name' => "Black Saturday", 'type' => "special-non-working", 'date' => "04-19", 'isActive' => true, 'isYearly' => false],
-            ['name' => "Christmas Eve", 'type' => "special-non-working", 'date' => "12-24", 'isActive' => true, 'isYearly' => true],
-            ['name' => "All Saints' Day Eve", 'type' => "special-non-working", 'date' => "10-31", 'isActive' => true, 'isYearly' => true],
+            ['name' => "Chinese New Year", 'type' => "special-non-working", 'date' => "01-29", 'isDeleted' => false, 'isYearly' => false],
+            ['name' => "Black Saturday", 'type' => "special-non-working", 'date' => "04-19", 'isDeleted' => false, 'isYearly' => false],
+            ['name' => "Christmas Eve", 'type' => "special-non-working", 'date' => "12-24", 'isDeleted' => false],
+            ['name' => "All Saints' Day Eve", 'type' => "special-non-working", 'date' => "10-31", 'isDeleted' => false],
         ];
         
         foreach ($holidays as $holiday) {
