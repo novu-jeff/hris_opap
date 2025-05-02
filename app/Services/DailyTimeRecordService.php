15d1c64595dbfcf7378bb91b843ee05a0cc34acb
Carl Llemos <iamcarlllemos@gmail.com>
2025-05-02 11:28:30 +0800

    latest

--- app/Services/DailyTimeRecordService.php
+++ app/Services/DailyTimeRecordService.php
@@ -198,6 +198,10 @@ class DailyTimeRecordService {
             $totalUndertimeDuration += $undertime;
             $totalLateDuration += $late;
     
+            # Check for discrepancy (if clock records are between 1 and 3)
+            if (count($clockEntry) >= 1 && count($clockEntry) <= 3) {
+                $remarks[] = 'Discrepancy';
+            }
         
             # Add data for the current day
             $mappedClockData[] = [
