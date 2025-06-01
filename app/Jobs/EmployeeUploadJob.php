fc3d650709b447df6f4505bcd7d806557d957cff
Carl Llemos <iamcarlllemos@gmail.com>
2025-06-02 01:03:12 +0800

    latest

--- app/Jobs/EmployeeUploadJob.php
+++ app/Jobs/EmployeeUploadJob.php
@@ -9,6 +9,8 @@ use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Bus\Batchable;
+use Illuminate\Support\Facades\Log;
+use Throwable;
 
 class EmployeeUploadJob implements ShouldQueue
 {
@@ -29,17 +31,27 @@ class EmployeeUploadJob implements ShouldQueue
     {
         $service = new EmployeeUploadService;
 
-        match ($this->sheetName) {
-            'Employee Information' => $service->uploadEmployeeInformation($this->chunk, $this->schedules),
-            'Family Background' => $service->uploadFamilyBackground($this->chunk),
-            'Children' => $service->uploadChildren($this->chunk),
-            'Education' => $service->uploadEducation($this->chunk),
-            'Employment History' => $service->uploadEmploymentHistory($this->chunk),
-            'Civil Service' => $service->uploadCivilService($this->chunk),
-            'Trainings' => $service->uploadTrainings($this->chunk),
-            'Other Works' => $service->uploadOtherWorks($this->chunk),
-            'Skills' => $service->uploadSkills($this->chunk),
-            default => null
-        };
+        try {
+            match ($this->sheetName) {
+                'Employee Information' => $service->uploadEmployeeInformation($this->chunk, $this->schedules),
+                'Family Background' => $service->uploadFamilyBackground($this->chunk),
+                'Children' => $service->uploadChildren($this->chunk),
+                'Education' => $service->uploadEducation($this->chunk),
+                'Employment History' => $service->uploadEmploymentHistory($this->chunk),
+                'Civil Service' => $service->uploadCivilService($this->chunk),
+                'Trainings' => $service->uploadTrainings($this->chunk),
+                'Other Works' => $service->uploadOtherWorks($this->chunk),
+                'Skills' => $service->uploadSkills($this->chunk),
+                default => null
+            };
+        } catch (Throwable $e) {
+            Log::error("Error processing sheet '{$this->sheetName}': " . $e->getMessage(), [
+                'sheet' => $this->sheetName,
+                'chunk' => $this->chunk,
+                'trace' => $e->getTraceAsString()
+            ]);
+            // Optionally, you can also mark this job as failed without retry:
+            // $this->fail($e);
+        }
     }
 }
