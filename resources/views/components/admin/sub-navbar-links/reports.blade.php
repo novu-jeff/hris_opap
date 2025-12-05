aeca983ec1350199a5a7346f33be301c3a86e76e
Jeff Pogi <jeff@novulutions.com>
2025-12-05 01:49:28 +0000

    test

--- resources/views/components/admin/sub-navbar-links/reports.blade.php
+++ resources/views/components/admin/sub-navbar-links/reports.blade.php
@@ -16,20 +16,7 @@
             <li><a href="{{ route('reports.philhealth') }}" class="dropdown-item">PhilHeath</a></li>
             <li><a href="{{ route('reports.sss') }}" class="dropdown-item">SSS</a></li>
             <li><a href="{{ route('reports.pagibig') }}" class="dropdown-item">Pagibig</a></li>
-            <li><a href="#" class="dropdown-item">Official Business Slip</a></li>
-            <li><a href="#" class="dropdown-item">Application for Leave</a></li>
-            <li><a href="#" class="dropdown-item">Monetization Form</a></li>
-            <li><a href="#" class="dropdown-item">Authority to Render Overtime</a></li>
-            <li><a href="#" class="dropdown-item">Statement of Overtime</a></li>
-            <li><a href="#" class="dropdown-item">Accomplishment Report</a></li>
-            <li><a href="#" class="dropdown-item">Overtime Work Program</a></li>
-            <li><a href="#" class="dropdown-item">Certificate of Clearance</a></li>
-            <li><a href="#" class="dropdown-item">Certificate of Completion of Service</a></li>
-            <li><a href="#" class="dropdown-item">New Employee Onboarding Checklist</a></li>
-            <li><a href="#" class="dropdown-item">Courtesy Call</a></li>
-            <li><a href="#" class="dropdown-item">Terms Of Reference</a></li>
-            <li><a href="#" class="dropdown-item">(TBD)</a></li>
-            <li><a href="#" class="dropdown-item">(TBD)</a></li> -->
+            
         @endif
     </ul>    
 </li>
