c773d450d8acba8cefbe749656f5cd3a71cca4f2
Jeff Pogi <jeff@novulutions.com>
2025-12-08 02:05:38 +0000

    fixed routes and meta tags

--- config/meta.php
+++ config/meta.php
@@ -3,34 +3,6 @@
 $provider = env('APP_PROVIDER');
 
 return [
-    'blitzdev' => [
-        'favicon' => '/img/favicon/blitzdev',
-        'company' => 'Novulutions Inc.',
-        'logo' => 'novulutions.png',
-        'tagline' => 'Transform It: Unify Your Data Silos',
-        'client_logo' => 'client-logo.png',
-        'email' => 'contact@blitzdev.com.ph',
-        'socials' => [
-            'website' => '',
-            'facebook' => '',
-            'linked_in' => '',
-            'youtube' => ''
-        ],
-        'services' => [
-            'Website Design',
-            'Full Stack Development',
-            'Cross Platform Development',
-            'Api Integration',
-        ],
-        'products' => [
-            'E-Boss',
-            'Blitz Ledger',
-            'Sofia Cart',
-            'Sofia Utilities',
-            'Knowledge Management System ',
-            'Document Management System'
-        ]
-    ],
     'novulutions' => [
         'favicon' => '/img/favicon/novulutions',
         'company' => 'Novulutions Inc.',
