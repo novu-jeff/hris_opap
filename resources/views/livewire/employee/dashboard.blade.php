fa7d9a33fdb252c11389fdd2ec3d67bbb98d213a
Reynaldo Magbuhat Jr <novu-rey@192.168.1.162>
2026-01-04 11:26:55 +0800

    update employee dashboard date hired

--- resources/views/livewire/employee/dashboard.blade.php
+++ resources/views/livewire/employee/dashboard.blade.php
@@ -339,7 +339,7 @@
             <div class="row mb-2">
                 <div class="col-6">Date Hired:</div>
                 <div class="col-6 text-end fw-bold">
-                    {{ \Carbon\Carbon::parse($dateHired)->format('F d, Y') }}
+                    {{ $dateHired == 'N/A'? 'N/A' : \Carbon\Carbon::parse($dateHired)->format('F d, Y') }}
                 </div>
             </div>
 
