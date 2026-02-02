311874293e349cc7d6ff23ecc025b7f9724669f7
Reynaldo Magbuhat Jr <novu-rey@Reynaldos-MacBook-Air.local>
2026-02-02 09:43:06 +0800

    update roles

--- resources/views/components/admin/sidebar-desk.blade.php
+++ resources/views/components/admin/sidebar-desk.blade.php
@@ -141,7 +141,7 @@
                 <a href="{{ route('reports.dtr') }}" class="submenu-item">
                     <i class="fa-solid fa-clipboard-list"></i> Daily Time Record</a>
                 @endcan
-                @can('read dtr')
+                @can('read payroll-report')
                 <a href="{{ route('reports.payroll') }}" class="submenu-item">
                     <i class="fa-solid fa-clipboard-list"></i> Payroll Record</a>
                 @endcan
