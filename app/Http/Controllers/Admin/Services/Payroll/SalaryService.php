7a0debdeffc51544254cbd4993fd60b432df6e4d
Reynaldo Magbuhat Jr <novu-rey@192.168.1.43>
2026-04-22 08:12:38 +0800

    add logs

--- app/Http/Controllers/Admin/Services/Payroll/SalaryService.php
+++ app/Http/Controllers/Admin/Services/Payroll/SalaryService.php
@@ -425,7 +425,7 @@ class SalaryService extends Controller {
       
                       }else{
                         $salaryBase = max($basic_salary, 10000);
-
+                        Log::info('Philhealth items', ['salaryBase' => $salaryBase, 'basicSalary' => $basic_salary]);
                         $philhealth = $hasDeductions
                             ? floor(($salaryBase * 0.05) * 100) / 100
                             : 0;
