52cb9f493042d5a244b5261191705d2bf18c49c7
Reynaldo Magbuhat Jr <novu-rey@192.168.1.3>
2026-06-28 09:15:37 +0800

    Update HRIS dashboard and leave management views

--- resources/views/livewire/admin/ess/leave/index.blade.php
+++ resources/views/livewire/admin/ess/leave/index.blade.php
@@ -177,7 +177,7 @@
                                 @forelse($records as $record)
                                     <tr data-id="{{$record->id}}">
                                         <td>{{$record->employee_no}}</td>
-                                        <td>{{ strtoupper(optional($record->employee->section)->name ?? '') }}</td>
+                                        <td>{{ strtoupper(optional($record->employee->section)->code ?? '') }}</td>
                                         <td>{{$record->employee->personal->firstname . ' ' . $record->employee->personal->lastname}}</td>
                                         <td>{{$record->leave_type->name}}</td>
                                         <td>{{ count($record->dates) }} {{ count($record->dates) === 1 ? 'Day' : 'Days' }}</td>
