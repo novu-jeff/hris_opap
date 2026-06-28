536f4e407965274bc0b98ac8e16498c3f495bf82
Reynaldo Magbuhat Jr <novu-rey@192.168.1.3>
2026-06-28 08:18:58 +0800

    Update leave management admin interface

--- resources/views/livewire/admin/ess/leave/index.blade.php
+++ resources/views/livewire/admin/ess/leave/index.blade.php
@@ -165,6 +165,7 @@
                             <thead>
                                 <tr>
                                     <th>Employee No</th>
+                                    <th>Unit</th>
                                     <th>Employee Name</th>
                                     <th>Leave Type</th>
                                     <th>Days Covered</th>
@@ -176,6 +177,7 @@
                                 @forelse($records as $record)
                                     <tr data-id="{{$record->id}}">
                                         <td>{{$record->employee_no}}</td>
+                                        <td>{{ strtoupper(optional($record->employee->section)->name ?? '') }}</td>
                                         <td>{{$record->employee->personal->firstname . ' ' . $record->employee->personal->lastname}}</td>
                                         <td>{{$record->leave_type->name}}</td>
                                         <td>{{ count($record->dates) }} {{ count($record->dates) === 1 ? 'Day' : 'Days' }}</td>
